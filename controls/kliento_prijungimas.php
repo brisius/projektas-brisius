cbabbe2840fe852ffb3276991512cfa9c76fcdfb
brisius <34135005+brisius@users.noreply.github.com>
2017-11-30 22:33:13 +0200

    Projektas-1
    Sukeliami projekto failai

---
+++ controls/kliento_prijungimas.php
@@ -0,0 +1,17 @@
+<?php
+
+include ('lib/klientai.php');
+$klientaiObj = new klientai();
+
+$data = array();
+
+if(!empty($_POST['submit'])){
+
+}
+
+
+//įtraukti registracijos šabloną
+include 'templates/prisijungimo_langas.html';
+
+
+?>
