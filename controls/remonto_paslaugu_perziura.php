356e55a17108cce0dc202f994db6318ec4918dc4
DESKTOP-T5JN11I\deividas <deividas.seimys@sneakybox.lt>
2017-12-03 08:13:24 +0200

    remonto paslaugu per

---
+++ controls/remonto_paslaugu_perziura.php
@@ -0,0 +1,24 @@
+<?php
+if(!isset($_SESSION)) 
+{ 
+    session_start(); 
+}
+include ('lib/remontai.php');
+$remontaiObj = new remontai();
+
+
+
+include 'utils/paging.php';
+
+$paging = new paging(9);
+
+$paging->process($elementCount, $pageId);
+
+$data = $remontaiObj->perziureti_paslaugas($paging->size, $paging->first);
+
+
+
+//Užkrauname registracijos langą
+include 'templates/remonto_paslaugu_perziura.html';
+?>
+
